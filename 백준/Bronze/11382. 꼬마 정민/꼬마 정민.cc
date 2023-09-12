8a4dca55dc6bd7c53fa42bbfdfc07b77194cb7bd
SangJin Choi <137066828+tkdwls97@users.noreply.github.com>
2023-09-12 23:58:33 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	
+	long long a = 0;
+	long long b = 0;
+	long long c = 0;
+	cin >> a >> b >> c;
+
+	cout << a + b + c;
+
+
+	return 0;
+}
