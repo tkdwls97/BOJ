5653de520fa55e72661fa4f8f77f764c70533e68
SangJin Choi <137066828+tkdwls97@users.noreply.github.com>
2024-04-23 18:18:16 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{
+
+
+
+	string str1 = {};
+	string str2 = "??!";
+
+	cin >> str1;
+
+
+	cout << str1 + str2;
+
+	
+
+	return 0;
+}
